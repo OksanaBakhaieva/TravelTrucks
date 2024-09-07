46f118ef347c52da0b310809784e3515bdb12080
OksanaBakhaieva <obahaeva@gmail.com>
2024-09-07 10:26:27 +0100

    install libraries, basic structure of project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './components/App/App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers";
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
